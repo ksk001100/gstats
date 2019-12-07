4ee9b1d877a0a41f2d5fbaa8ab44d1656ecfbc8d
KeisukeToyota <hm.pudding0715@gmail.com>
2019-12-08 02:33:07 +0900

    commit

--- src/lib/github/api.rs
+++ src/lib/github/api.rs
@@ -3,9 +3,20 @@ use serde_json;
 
 use crate::lib::github::structs::release::Releases;
 
-pub fn release(owner_repo: &str) -> Releases {
-    let url = format!("https://api.github.com/repos/{}/releases", owner_repo);
-    let mut res = reqwest::get(&url).unwrap();
-    let releases: Releases = serde_json::from_str(&res.text().unwrap()).unwrap();
-    releases
+pub struct Api {
+    token: String,
+}
+
+impl Api {
+    pub fn new(token: String) -> Self {
+        Self { token }
+    }
+
+    pub fn release(&self, owner_repo: &str) -> Result<Releases, Box<std::error::Error>> {
+        let url = format!("https://api.github.com/repos/{}/releases", owner_repo);
+        let mut res = reqwest::get(&url).unwrap();
+        let releases = serde_json::from_str(&res.text().unwrap())?;
+
+        Ok(releases)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ mod lib;
 
 use std::env;
 
-use lib::github::api;
+use lib::github::api::Api;
+use lib::color;
 use lib::traits::Statistics;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    api::release(&args[1]).stats();
+    let api = Api::new("".to_string());
+
+    match api.release(&args[1]) {
+        Ok(releases) => releases.stats(),
+        Err(_) => println!("{}", color::red("Repository does not exist..."))
+    }
 }
