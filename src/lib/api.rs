9b26fd4a2aef95610a8e6b811982112ea1ea31f1
KeisukeToyota <hm.pudding0715@gmail.com>
2019-12-08 00:48:37 +0900

    commit

--- src/lib/api.rs
+++ src/lib/api.rs
@@ -1,22 +1,6 @@
 use reqwest;
 
-use crate::lib::color;
 use crate::lib::structs::{Releases};
-use crate::lib::traits::{Statistics};
-
-impl Statistics for Releases {
-    fn stats(&self) {
-        let mut total_count = 0;
-        for release in self {
-            let count = release.download_count();
-            total_count = total_count + count;
-            let s = format!("{} | {}", release.tag_name, color::yellow(count));
-            println!("{}", s);
-            println!("{}", "-".to_string().repeat(s.len()));
-        }
-        println!("Total  | {}", color::red(total_count));
-    }
-}
 
 pub fn release(owner_repo: &str) -> Releases {
     let url = format!("https://api.github.com/repos/{}/releases", owner_repo);
--- src/lib/structs.rs
+++ src/lib/structs.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
-use serde_json::{self, Result};
+use serde_json;
+
+use crate::lib::color;
+use crate::lib::traits::{Statistics};
 
 #[derive(Serialize, Deserialize)]
 pub struct Release {
@@ -20,3 +23,17 @@ impl Release {
 }
 
 pub type Releases = Vec<Release>;
+
+impl Statistics for Releases {
+    fn stats(&self) {
+        let mut total_count = 0;
+        for release in self {
+            let count = release.download_count();
+            total_count = total_count + count;
+            let s = format!("{} | {}", release.tag_name, color::yellow(count));
+            println!("{}", s);
+            println!("{}", "-".to_string().repeat(s.len()));
+        }
+        println!("Total  | {}", color::red(total_count));
+    }
+}
