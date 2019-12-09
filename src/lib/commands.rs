73f25dd176bd45687a5e516f0131c1dd588fd974
KeisukeToyota <hm.pudding0715@gmail.com>
2019-12-09 20:28:48 +0900

    commit

--- src/lib/commands.rs
+++ src/lib/commands.rs
@@ -1,14 +1,19 @@
-use seahorse::command::Command;
+use seahorse::Command;
 use seahorse::color;
 
 use crate::lib::github::api::Api;
 use crate::lib::traits::Statistics;
 
 pub fn release_command() -> Command {
-    Command{name: "release".to_string(), action: release_action}
+    Command {
+        name: "release".to_string(),
+        usage: "gstats release [Owner/Repo]".to_string(),
+        action: release_action,
+    }
 }
 
-fn release_action(owner_repo: String) {
+fn release_action(args: Vec<String>) {
+    let owner_repo = args[0].clone();
     let api = Api::new("".to_string());
 
     match api.release(&owner_repo) {
